25670c16222efd158fe11374f41e57c8baa07c5b
Blaž Hrastnik <blaz@mxxn.io>
2020-11-21 16:34:43 +0900

    Convert neovim to proper overlay.

--- overlays/neovim.nix
+++ overlays/neovim.nix
@@ -1,15 +1,16 @@
-{ neovim-unwrapped, fetchFromGitHub, tree-sitter }:
-
+final: prev:
 let
   rev = "97ffa158aa9ae6c80a48b3ceea91270f0a179743";
   sha256 = "10qplh8a7pndr5wv5irkill569af2hka7cwrlrm2cbn0ix6bz65z";
 in
-  neovim-unwrapped.overrideAttrs(old: {
+  {
+   neovim-unwrapped = prev.neovim-unwrapped.overrideAttrs(old: {
     version = "0.5.0-${rev}";
-    src = fetchFromGitHub {
+    src = prev.fetchFromGitHub {
       owner = "neovim";
       repo = "neovim";
       inherit rev sha256;
     };
-    buildInputs = old.buildInputs ++ [ tree-sitter ];
-  })
+    buildInputs = old.buildInputs ++ [ prev.tree-sitter ];
+  });
+}
--- pkgs/default.nix
+++ pkgs/default.nix
@@ -1,9 +1,6 @@
 final: prev: {
   # https://github.com/colemickens/nixpkgs-wayland/issues/233
   rofi-wayland = prev.callPackage ./applications/rofi-wayland.nix { };
-  neovim-unwrapped = prev.callPackage ./applications/neovim.nix {
-    neovim-unwrapped = prev.neovim-unwrapped;
-  };
   # add the surface-aggregator kernel module to all kernel definitions
   linuxPackagesFor = kernel:
     (prev.linuxPackagesFor kernel).extend (final': prev': {
