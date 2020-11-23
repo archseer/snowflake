617e62483cead66d085d62d7bd57327a11db07d0
Blaž Hrastnik <blaz@mxxn.io>
2020-11-23 14:02:13 +0900

    colibri: Fix sha256

--- pkgs/development/colibri-vim.nix
+++ pkgs/development/colibri-vim.nix
@@ -6,7 +6,7 @@ pkgs.vimUtils.buildVimPluginFrom2Nix {
     owner = "archseer";
     repo = "colibri.vim";
     rev = "ad82132e0cbbdfa194d722f15c2df8f0d04b5b71";
-    sha256 = "0r4xs0mhdxvac81cly89jqnby14h1dmrpkdfs0chz5ji4gbsgair";
+    sha256 = "1aknwa8gacinvfq9vnpqx3xjwjl8qddih8cvf878nf2xhp59gff0";
   };
   meta.homepage = "https://github.com/archseer/colibri.vim";
 }
