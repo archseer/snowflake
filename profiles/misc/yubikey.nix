b1a3f0e562381c4bc03b86087e84b8c9c17b2272
Blaž Hrastnik <blaz@mxxn.io>
2020-12-14 15:40:57 +0900

    fix u2f_keys format to properly pick up the second key.

--- profiles/misc/yubikey.nix
+++ profiles/misc/yubikey.nix
@@ -21,8 +21,7 @@
 
     # create key: `pamu2fcfg`
     authFile = pkgs.writeText "u2f_keys" ''
-    speed:BrgFsoVXT5i6cn9gU/qy9wS7uhFnR/5KceLJQFP3+SmQ+0g1chxXYzpWwzeDhyBBYMBpsyEmtRKgdOQrhGXhLA==,3OktBosHbWOwbJXWkG0g0k/Yymh6eSgVwHH8/jECWnnTNG5me26sU8hkCxdfS2BpPGJLasQxT2OHg8ql6MoeuA==,es256,+presence
-    speed:1BaBxWeS2ClIpyWlrEpTJylYeXNl9qNAvIeHaD28t5HFK5iRWOsy2VY+KW9WkMnr2yrM8dXp3U9cVJiG6XjqaQ==,VR6wUpt67GGZD6r7cQKcF3DqM/MLR5lXguMy3jaQkcB/YtLqaEIrzEThXZFco4TAIlodjlTDk98Sy3DiaeeD1w==,es256,+presence
+    speed:BrgFsoVXT5i6cn9gU/qy9wS7uhFnR/5KceLJQFP3+SmQ+0g1chxXYzpWwzeDhyBBYMBpsyEmtRKgdOQrhGXhLA==,3OktBosHbWOwbJXWkG0g0k/Yymh6eSgVwHH8/jECWnnTNG5me26sU8hkCxdfS2BpPGJLasQxT2OHg8ql6MoeuA==,es256,+presence:1BaBxWeS2ClIpyWlrEpTJylYeXNl9qNAvIeHaD28t5HFK5iRWOsy2VY+KW9WkMnr2yrM8dXp3U9cVJiG6XjqaQ==,VR6wUpt67GGZD6r7cQKcF3DqM/MLR5lXguMy3jaQkcB/YtLqaEIrzEThXZFco4TAIlodjlTDk98Sy3DiaeeD1w==,es256,+presence
     '';
   };
 }
