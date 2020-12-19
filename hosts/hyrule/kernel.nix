c7d50113147a7caec7d4bf65d85c4448bc1ee719
Blaž Hrastnik <blaz@mxxn.io>
2020-12-19 14:06:13 +0900

    kernel: Set touchpad to high latency on suspend.

--- hosts/hyrule/kernel.nix
+++ hosts/hyrule/kernel.nix
@@ -4,10 +4,16 @@
 (linuxManualConfig {
   inherit (pkgs.linux_latest) stdenv version src;
   configfile = ./kernel.config;
-  kernelPatches = [{
-    name = "iio";
-    patch = ./iio.patch;
-  }]; # TODO: pass through kernelPatches
+  kernelPatches = [
+    {
+      name = "iio";
+      patch = ./iio.patch;
+    }
+      {
+      name = "mt-suspend";
+      patch = ./mt-suspend.patch;
+    }
+  ]; # TODO: pass through kernelPatches
   allowImportFromDerivation = true;
 })
 .overrideAttrs(o: { nativeBuildInputs = o.nativeBuildInputs ++ [ pkgs.zstd ]; }) # for zstd compression
