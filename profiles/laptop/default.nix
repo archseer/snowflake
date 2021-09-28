149634946068935c3b4f09143ac16115b742933e
Blaž Hrastnik <blaz@mxxn.io>
2021-09-28 18:21:05 +0900

    Fix interception-tools

--- profiles/laptop/default.nix
+++ profiles/laptop/default.nix
@@ -8,7 +8,16 @@
   ];
 
   # remap caps to escape/ctrl on built-in keyboard.
-  services.interception-tools.enable = true;
+  services.interception-tools = {
+    enable = true;
+    plugins = [ pkgs.interception-tools-plugins.caps2esc ];
+    udevmonConfig = ''
+    - JOB: "${pkgs.interception-tools}/bin/intercept -g $DEVNODE | ${pkgs.interception-tools-plugins.caps2esc}/bin/caps2esc | ${pkgs.interception-tools}/bin/uinput -d $DEVNODE"
+      DEVICE:
+        EVENTS:
+          EV_KEY: [KEY_CAPSLOCK, KEY_ESC]
+    '';
+  };
 
   services.upower.enable = lib.mkDefault true;
 
