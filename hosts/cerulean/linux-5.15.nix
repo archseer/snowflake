1e4e39ef36c028c96f7efdc3689d4de6da18c40a
Blaž Hrastnik <blaz@mxxn.io>
2021-11-07 14:35:17 +0900

    cerulean: kernel: 5.15.1

--- hosts/cerulean/linux-5.15.nix
+++ hosts/cerulean/linux-5.15.nix
@@ -3,7 +3,7 @@
 with lib;
 
 buildLinux (args // rec {
-  version = "5.15";
+  version = "5.15.1";
 
   # modDirVersion needs to be x.y.z, will automatically add .0 if needed
   modDirVersion = if (modDirVersionArg == null) then concatStringsSep "." (take 3 (splitVersion "${version}.0")) else modDirVersionArg;
@@ -13,6 +13,6 @@ buildLinux (args // rec {
 
   src = fetchurl {
     url = "mirror://kernel/linux/kernel/v5.x/linux-${version}.tar.xz";
-    sha256 = "sha256-V7LPaZGRDjtnobNJACLooGdLaWXHTBLaHpnRONGZHug=";
+    sha256 = "sha256-Mv3NM8isVxuaeil/M4YPYXEyeWHyoupr1Uv4InW2FMg=";
   };
 } // (args.argsOverride or { }))
