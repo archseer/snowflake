be365e08258b5825fb9d5e9d014be6bad1f100d5
Blaž Hrastnik <blaz@mxxn.io>
2022-03-16 12:47:31 +0900

    Add udev rules for qmk

--- profiles/develop/default.nix
+++ profiles/develop/default.nix
@@ -3,6 +3,15 @@
   home-manager.users.speed = {
     imports = [ ./neovim ./tmux ];
   };
+  
+  # qmk rules
+  services.udev.extraRules = ''
+    # UDEV rules for Teensy USB devices
+    ATTRS{idVendor}=="16c0", ATTRS{idProduct}=="04[789B]?", ENV{ID_MM_DEVICE_IGNORE}="1"
+    ATTRS{idVendor}=="16c0", ATTRS{idProduct}=="04[789A]?", ENV{MTP_NO_PROBE}="1"
+    SUBSYSTEMS=="usb", ATTRS{idVendor}=="16c0", ATTRS{idProduct}=="04[789ABCD]?", MODE:="0666"
+    KERNEL=="ttyACM*", ATTRS{idVendor}=="16c0", ATTRS{idProduct}=="04[789B]?", MODE:="0666"
+  '';
 
   environment = {
     sessionVariables = {
