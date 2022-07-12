94321a58d4921fbf1de69e49418ba3ab612fe7cf
Blaž Hrastnik <blaz@mxxn.io>
2022-07-12 16:13:27 +0900

    Experimenting with Steam

--- profiles/graphical/games/default.nix
+++ profiles/graphical/games/default.nix
@@ -1,29 +1,35 @@
 { pkgs, ... }: {
   imports = [ ./udev.nix ];
+
+  programs.steam = {
+    enable = true;
+    remotePlay.openFirewall = true; # Open ports in the firewall for Steam Remote Play
+    dedicatedServer.openFirewall = true; # Open ports in the firewall for Source Dedicated Server
+  };
+  
+  
+  hardware.opengl.driSupport32Bit = true;
+  hardware.opengl.enable = true;
+  hardware.pulseaudio.support32Bit = true;
   environment.systemPackages = with pkgs; [
-    retroarchBare
-    steam-run
-    pcsx2
-    qjoypad
+    protontricks
+    proton-caller
+    python3
+    # retroarchBare
+    # pcsx2
+    # qjoypad
+    vulkan-tools
+    vulkan-loader
   ];
 
-  services.wii-u-gc-adapter.enable = true;
+  # services.wii-u-gc-adapter.enable = true;
 
   # fps games on laptop need this
-  services.xserver.libinput.disableWhileTyping = false;
-
-  # Launch steam from display managers
-  services.xserver.windowManager.steam = { enable = true; };
-
-  # 32-bit support needed for steam
-  hardware.opengl.driSupport32Bit = true;
-  hardware.pulseaudio.support32Bit = true;
-
-  hardware.steam-hardware.enable = true;
+  # services.xserver.libinput.disableWhileTyping = false;
 
   # better for steam proton games
   systemd.extraConfig = "DefaultLimitNOFILE=1048576";
 
   # improve wine performance
-  environment.sessionVariables = { WINEDEBUG = "-all"; };
+  # environment.sessionVariables = { WINEDEBUG = "-all,fixme-all"; };
 }
