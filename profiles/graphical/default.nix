1899c351b34b207efc36e74a346c3303b1204bf3
Blaž Hrastnik <blaz@mxxn.io>
2022-08-02 22:15:49 +0900

    gtkUsePortal is deprecated

--- profiles/graphical/default.nix
+++ profiles/graphical/default.nix
@@ -62,7 +62,6 @@ in
   xdg.portal = {
     enable = true;
     extraPortals = with pkgs; [ xdg-desktop-portal-wlr xdg-desktop-portal-gtk ];
-    gtkUsePortal = true;
   };
 
   programs.sway.extraSessionCommands = lib.mkBefore ''
