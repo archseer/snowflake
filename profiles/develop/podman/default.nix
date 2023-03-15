03aa99cbed580d25e555144ebfec601ddf1a19da
Blaž Hrastnik <blaz@mxxn.io>
2023-03-15 22:46:04 +0900

    networking: netavark fails with k3d for now, revert to cni

--- profiles/develop/podman/default.nix
+++ profiles/develop/podman/default.nix
@@ -1,12 +1,51 @@
-{pkgs, ...}: {
+{lib, pkgs, ...}: {
   virtualisation.docker.enable = false;
   virtualisation.podman = {
     enable = true;
     dockerCompat = true;
     dockerSocket.enable = true;
-    defaultNetwork.settings.dns_enabled = true;
+    defaultNetwork.settings = lib.mkForce {};
+    # defaultNetwork.settings.dns_enabled = true;
   };
   virtualisation.oci-containers.backend = "podman";
+
+  # k3d seems broken on netavark
+  virtualisation.containers.containersConf.settings = {
+    network.network_backend = lib.mkForce "cni";
+  };
+  virtualisation.containers.containersConf.cniPlugins = [ pkgs.dnsname-cni ];
+  environment.etc."cni/net.d/87-podman-bridge.conflist".text = builtins.toJSON {
+    cniVersion = "0.4.0";
+    name = "podman";
+    plugins = [{
+      type =  "bridge";
+      bridge = "cni-podman0";
+      isGateway = true;
+      ipMasq = true;
+      hairpinMode = true;
+      ipam = {
+        type = "host-local";
+        routes = [{ dst = "0.0.0.0/0"; }];
+        ranges = [
+          [
+            { subnet = "10.88.0.0/16"; gateway = "10.88.0.1"; }
+          ]
+        ];
+      };
+    }
+    {
+      type = "portmap";
+      capabilities = {
+        portMappings = true;
+      };
+    }
+    {
+      type = "firewall";
+    }
+    {
+      type = "tuning";
+    }];
+  };
 
   # environment.systemPackages = [ pkgs.docker-client ];
   # environment.systemPackages = [
