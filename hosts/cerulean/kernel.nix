8a979d864534aaa96a474b5f8f290eb85cdc0015
Blaž Hrastnik <blaz@mxxn.io>
2023-07-10 11:37:12 +0900

    Update kernel to 6.4.2

--- hosts/cerulean/kernel.nix
+++ hosts/cerulean/kernel.nix
@@ -9,7 +9,7 @@
   linuxPackagesFor,
   ...
 }: let
-  linux = pkgs.linuxKernel.kernels.linux_6_3;
+  linux = pkgs.linuxKernel.kernels.linux_6_4;
   # linux = pkgs.callPackage ./linux-6.1.nix {};
 
   kernel =
