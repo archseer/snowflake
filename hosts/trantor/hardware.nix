586b1d7532696d384ada3352af49f70c07895722
Blaž Hrastnik <blaz@mxxn.io>
2023-08-08 14:02:35 +0900

    Attempt to fix DM_CRYPT config

--- hosts/trantor/hardware.nix
+++ hosts/trantor/hardware.nix
@@ -19,23 +19,7 @@
 
   # upstream includes SATA drivers etc. which we don't build into the kernel.
   boot.initrd.includeDefaultModules = false;
-  boot.initrd.availableKernelModules = lib.mkForce ["nvme" "xhci_pci" "ahci" "usbhid" "usb_storage" "sd_mod"];
-  # boot.initrd.availableKernelModules = lib.mkForce [
-  #   "xhci_pci"
-  #   "nvme"
-  #   "sd_mod"
-  #   "dm_mod"
-  #   "dm_crypt"
-  #   "cryptd"
-  #   # required for keyboard support at init
-  #   "intel_lpss"
-  #   "intel_lpss_pci"
-  #   "8250_dw"
-  #   "surface_aggregator"
-  #   "surface_aggregator_registry"
-  #   "surface_hid_core"
-  #   "surface_hid"
-  # ];
+  boot.initrd.availableKernelModules = lib.mkForce ["nvme" "xhci_pci" "ahci" "usbhid" "usb_storage" "sd_mod" "dm_mod" "dm_crypt" "cryptd"];
 
   boot.kernelModules = ["kvm-intel"];
 
