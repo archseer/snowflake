c983f429ab7c6998792dec8d72bba6f51e304c6a
Blaž Hrastnik <blaz@mxxn.io>
2024-01-23 15:39:31 +0900

    kernel: Update to 6.7.1

--- hosts/trantor/kernel.nix
+++ hosts/trantor/kernel.nix
@@ -8,7 +8,7 @@
   kernelPatches,
   ...
 }: let
-  linux = pkgs.linuxKernel.kernels.linux_6_6;
+  linux = pkgs.linuxKernel.kernels.linux_6_7;
   # linux = pkgs.callPackage ./linux-6.1.nix {};
 
   kernel =
