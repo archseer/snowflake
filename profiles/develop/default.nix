f1138649cf222b98c6a00622bc719f5857f06c98
Blaž Hrastnik <blaz@mxxn.io>
2026-05-14 18:12:19 +0900

    Drop neovim config

--- profiles/develop/default.nix
+++ profiles/develop/default.nix
@@ -1,7 +1,7 @@
 {pkgs, ...}: {
   imports = [./zsh ./podman];
   home-manager.users.speed = {
-    imports = [./neovim ./tmux ./wezterm ./alacritty];
+    imports = [./tmux ./wezterm ./alacritty];
   };
 
   # qmk rules
--- profiles/develop/neovim/default.nix
+++
@@ -1,7 +0,0 @@
-{pkgs, ...}: {
-  programs.neovim = {
-    enable = true;
-    plugins = with pkgs.vimPlugins; [ vim-repeat vim-abolish ];
-    extraConfig = builtins.readFile ./init.vim;
-  };
-}
